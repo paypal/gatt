fde8a01e419bf6ee2a30ed17290308b27e428a98
Tzu-Jung Lee <roylee17@gmail.com>
2014-11-27 10:00:32 -0800

    move notifier from characteristic to conn

--- notifier.go
+++ notifier.go
@@ -1,54 +1,25 @@
 package gatt
 
-import (
-	"errors"
-	"sync"
-	"time"
-)
+import "errors"
 
 type notifier struct {
-	l2c    *l2cap
+	conn   *conn
 	char   *Characteristic
 	maxlen int
-	donemu sync.RWMutex
 	done   bool
-	// This throttle prevents multiple subsequent notifications from
-	// stepping on each others' toes. This toe-stepping appears to
-	// happen at both the HCI and the link layer.
-	throttle *time.Ticker
 }
 
-func newNotifier(l2c *l2cap, c *Characteristic, maxlen int) *notifier {
-	return &notifier{
-		l2c:      l2c,
-		char:     c,
-		maxlen:   maxlen,
-		throttle: time.NewTicker(50 * time.Millisecond),
-	}
+func newNotifier(c *conn, cc *Characteristic, maxlen int) *notifier {
+	return &notifier{conn: c, char: cc, maxlen: maxlen}
 }
 
 func (n *notifier) Write(data []byte) (int, error) {
 	if n.Done() {
 		return 0, errors.New("central stopped notifications")
 	}
-	<-n.throttle.C
-	return n.l2c.server.conn.sendNotification(n.char, data)
+	return n.conn.sendNotification(n.char, data)
 }
 
-func (n *notifier) Cap() int {
-	return n.maxlen
-}
-
-func (n *notifier) Done() bool {
-	n.donemu.RLock()
-	done := n.done
-	n.donemu.RUnlock()
-	return done
-}
-
-func (n *notifier) stop() {
-	n.donemu.Lock()
-	n.done = true
-	n.donemu.Unlock()
-	n.throttle.Stop()
-}
+func (n *notifier) Cap() int   { return n.maxlen }
+func (n *notifier) Done() bool { return n.done }
+func (n *notifier) stop()      { n.done = true }
