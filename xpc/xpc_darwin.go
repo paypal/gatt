b67cf09ad578b6b17995544be1d66e15b011667e
Raffaele Sena <raff367@gmail.com>
2015-05-17 13:55:54 -0700

    Starting with OSX Yosemite the XPC message for IBeacon advertising has changed. This patch check the OS version and send the appropriate message.

--- xpc/xpc_darwin.go
+++ xpc/xpc_darwin.go
@@ -2,6 +2,7 @@ package xpc
 
 /*
 #include "xpc_wrapper_darwin.h"
+#include "sys/utsname.h"
 */
 import "C"
 
@@ -320,4 +321,30 @@ func xpcToGo(v C.xpc_object_t) interface{} {
 // xpc_release is needed by tests, since they can't use CGO
 func xpc_release(xv C.xpc_object_t) {
 	C.xpc_release(xv)
+}
+
+// this is used to check the OS version
+
+type Utsname struct {
+       Sysname  string
+       Nodename string
+       Release  string
+       Version  string
+       Machine  string
+ }
+
+func Uname(utsname *Utsname) error {
+      var cstruct C.struct_utsname
+      if err := C.uname(&cstruct); err != 0 {
+              return errors.New("utsname error")
+      }
+
+      // XXX: this may crash if any value is exactly 256 characters (no 0 terminator)
+      utsname.Sysname = C.GoString(&cstruct.sysname[0])
+      utsname.Nodename = C.GoString(&cstruct.nodename[0])
+      utsname.Release = C.GoString(&cstruct.release[0])
+      utsname.Version = C.GoString(&cstruct.version[0])
+      utsname.Machine = C.GoString(&cstruct.machine[0])
+
+      return nil
 }
