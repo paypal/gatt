d11f416142c55ebc37879dd35c5e98a36cd9038e
Tzu-Jung Lee <roylee@paypal.com>
2015-09-16 21:59:23 -0700

    gofmt: xpc/xpc_darwin.go

--- xpc/xpc_darwin.go
+++ xpc/xpc_darwin.go
@@ -326,25 +326,25 @@ func xpc_release(xv C.xpc_object_t) {
 // this is used to check the OS version
 
 type Utsname struct {
-       Sysname  string
-       Nodename string
-       Release  string
-       Version  string
-       Machine  string
- }
+	Sysname  string
+	Nodename string
+	Release  string
+	Version  string
+	Machine  string
+}
 
 func Uname(utsname *Utsname) error {
-      var cstruct C.struct_utsname
-      if err := C.uname(&cstruct); err != 0 {
-              return errors.New("utsname error")
-      }
+	var cstruct C.struct_utsname
+	if err := C.uname(&cstruct); err != 0 {
+		return errors.New("utsname error")
+	}
 
-      // XXX: this may crash if any value is exactly 256 characters (no 0 terminator)
-      utsname.Sysname = C.GoString(&cstruct.sysname[0])
-      utsname.Nodename = C.GoString(&cstruct.nodename[0])
-      utsname.Release = C.GoString(&cstruct.release[0])
-      utsname.Version = C.GoString(&cstruct.version[0])
-      utsname.Machine = C.GoString(&cstruct.machine[0])
+	// XXX: this may crash if any value is exactly 256 characters (no 0 terminator)
+	utsname.Sysname = C.GoString(&cstruct.sysname[0])
+	utsname.Nodename = C.GoString(&cstruct.nodename[0])
+	utsname.Release = C.GoString(&cstruct.release[0])
+	utsname.Version = C.GoString(&cstruct.version[0])
+	utsname.Machine = C.GoString(&cstruct.machine[0])
 
-      return nil
+	return nil
 }
